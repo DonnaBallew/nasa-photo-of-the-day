e5d20b60659fd3145212738ee29fbf5550ae6a79
Donna Ballew <ballewdd@gmail.com>
2020-05-25 16:47:11 -0700

    completed NasaPhoto component

---
+++ src/components/NasaPhoto.js
@@ -0,0 +1,27 @@
+import React, { useState, useEffect } from "react";
+import { NasaCard } from "./NasaCard";
+import axios from "axios";
+
+export default function NasaPhoto(props) {
+  const [info, setInfo] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get(
+        "https://api.nasa.gov/planetary/apod?api_key=Cq2LeY2J2jVpRURbwuqcMMbyJQbCKyCxynPr6pPo"
+      )
+      .then((res) => {
+        console.log(res);
+        setInfo(res.data);
+      })
+      .catch((error) => {
+        console.log("Data is lost in a black hole!", error);
+      });
+  }, []);
+
+  return (
+    <Container className="info">
+      <NasaCard />
+    </Container>
+  );
+}
